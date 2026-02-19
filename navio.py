ecc59a57126450db919068024d64a3096cfa105e
King_Kong24 <jfbianco2410@gmail.com>
2026-02-19 21:51:33 +0000

    Implementação das classes navio and tripulante

--- navio.py
+++ navio.py
@@ -1,2 +1,16 @@
 class navio:
+    def __init__(self, nome, tripulação):
+        self.__nome = nome
+        self.__tripulação = tripulação
+
+    def recrutar(novo_tripulante):
+        self.__tripulação.append(novo_tripulante)
+                                 
+    def expulsar(nome_tripulante):
     
+
+    def calcular_poder_total():
+        pass
+        
+    def mostrar_manifesto():
+        pass
--- tripulante.py
+++ tripulante.py
@@ -1,2 +1,20 @@
 class tripulante:
+    def __init__(self, nome, funcao, recompensa, poder, energia):
+        self.__nome = nome
+        self.__funcao = funcao
+        self.__recompensa = recompensa
+        self.__poder = poder
+        self.__energia = energia
+
+    def trabalhar(tempo):
+        self.__energia -= tempo * 5
+        if self.__energia < 0:
+            self.__energia = 0
     
+    def descansar():
+        
+        if self.__energia > 100:
+            self.__energia = 100
+
+    def __str__(self):
+        return f"Tripulante: {self.__nome}, Função: {self.__funcao}, Recompensa: {self.__recompensa}, Poder: {self.__poder}, Energia: {self.__energia}"
